b6155912ac28d7e07758596afed697efdd8ca0dd
SACHIN <sachinshanmugam07@gmail.com>
2021-11-26 17:29:42 +0530

    GENDER_DEFAULT value changed in index.jss

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const GENDER_DEFAULT='Female';
+const GENDER_DEFAULT='Male';
 
 ReactDOM.render(
 
